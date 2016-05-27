d750fad7f7beb46a8d35c7b0546300db55be7563
Eito Katagiri <eitoball@gmail.com>
2016-05-28 08:46:54 +0900

    load nil, true, false, and Fixnum

--- lib/ruby_marshal.ex
+++ lib/ruby_marshal.ex
@@ -1,11 +1,19 @@
 defmodule RubyMarshal do
-  @version_header <<4, 8>>
+  def load(<<4, 8, 48>>), do: nil
+  def load(<<4, 8, 84>>), do: true
+  def load(<<4, 8, 70>>), do: false
 
-  def dump(val) when is_nil(val) do
-    @version_header <> "0"
-  end
+  def load(<<4, 8, 105, _::binary>> = src), do: load_fixnum(src)
 
-  def dump(val) when is_boolean(val) do
-    @version_header <> if(val, do: "T", else: "F")
-  end
+  defp load_fixnum(<<4, 8, 105, n::signed-size(8)>>) when n == 0, do: 0
+  defp load_fixnum(<<4, 8, 105, n::signed-size(8)>>) when n in 6..127, do: n - 5
+  defp load_fixnum(<<4, 8, 105, n::signed-size(8)>>) when n in -128..-1, do: n + 5
+  defp load_fixnum(<<4, 8, 105, 1, n::unsigned-size(8)>>), do: n
+  defp load_fixnum(<<4, 8, 105, 2, n::little-unsigned-size(16)>>), do: n
+  defp load_fixnum(<<4, 8, 105, 3, n::little-unsigned-size(24)>>), do: n
+  defp load_fixnum(<<4, 8, 105, 4, n::little-unsigned-size(32)>>), do: n
+  defp load_fixnum(<<4, 8, 105, 255, n::unsigned-size(8)>>), do: n - 0x100
+  defp load_fixnum(<<4, 8, 105, 254, n::little-unsigned-size(16)>>), do: n - 0x10000
+  defp load_fixnum(<<4, 8, 105, 253, n::little-unsigned-size(24)>>), do: n - 0x1000000
+  defp load_fixnum(<<4, 8, 105, 252, n::little-unsigned-size(32)>>), do: n - 0x100000000
 end
--- test/ruby_marshal_test.exs
+++ test/ruby_marshal_test.exs
@@ -2,15 +2,99 @@ defmodule RubyMarshalTest do
   use ExUnit.Case
   doctest RubyMarshal
 
-  test "dump nil" do
-    assert RubyMarshal.dump(nil) == "\x04\b0"
+  test "load nil" do
+    assert RubyMarshal.load(<<4, 8, 48>>) == nil
   end
 
-  test "dump true" do
-    assert RubyMarshal.dump(true) == "\x04\bT"
+  test "load true" do
+    assert RubyMarshal.load(<<4, 8, 84>>) == true
   end
 
-  test "dump false" do
-    assert RubyMarshal.dump(false) == "\x04\bF"
+  test "load false" do
+    assert RubyMarshal.load(<<4, 8, 70>>) == false
+  end
+
+  test "load Fixnum 0" do
+    assert RubyMarshal.load(<<4, 8, 105, 0>>) == 0
+  end
+
+  test "load Fixnum 1" do
+    assert RubyMarshal.load(<<4, 8, 105, 6>>) == 1
+  end
+
+  test "load Fixnum 122" do
+    assert RubyMarshal.load(<<4, 8, 105, 127>>) == 122
+  end
+
+  test "load Fixnum -1" do
+    assert RubyMarshal.load(<<4, 8, 105, 250>>) == -1
+  end
+
+  test "load Fixnum -123" do
+    assert RubyMarshal.load(<<4, 8, 105, 128>>) == -123
+  end
+
+  test "load Fixnum 123" do
+    assert RubyMarshal.load(<<4, 8, 105, 1, 123>>) == 123
+  end
+
+  test "load Fixnum 255" do
+    assert RubyMarshal.load(<<4, 8, 105, 1, 255>>) == 255
+  end
+
+  test "load Fixnum 256" do
+    assert RubyMarshal.load(<<4, 8, 105, 2, 0, 1>>) == 256
+  end
+
+  test "load Fixnum 65535" do
+    assert RubyMarshal.load(<<4, 8, 105, 2, 255, 255>>) == 65535
+  end
+
+  test "load Fixnum 65536" do
+    assert RubyMarshal.load(<<4, 8, 105, 3, 0, 0, 1>>) == 65536
+  end
+
+  test "load Fixnum 16777215" do
+    assert RubyMarshal.load(<<4, 8, 105, 3, 255, 255, 255>>) == 16777215
+  end
+
+  test "load Fixnum 16777216" do
+    assert RubyMarshal.load(<<4, 8, 105, 4, 0, 0, 0, 1>>) == 16777216
+  end
+
+  test "load Fixnum 4294967295" do
+    assert RubyMarshal.load(<<4, 8, 105, 4, 255, 255, 255, 255>>) == 4294967295
+  end
+
+  test "load Fixnum -124" do
+    assert RubyMarshal.load(<<4, 8, 105, 255, 132>>) == -124
+  end
+
+  test "load Fixnum -256" do
+    assert RubyMarshal.load(<<4, 8, 105, 255, 0>>) == -256
+  end
+
+  test "load Fixnum -257" do
+    assert RubyMarshal.load(<<4, 8, 105, 254, 255, 254>>) == -257
+  end
+
+  test "load Fixnum -65536" do
+    assert RubyMarshal.load(<<4, 8, 105, 254, 0, 0>>) == -65536
+  end
+
+  test "load Fixnum -65537" do
+    assert RubyMarshal.load(<<4, 8, 105, 253, 255, 255, 254>>) == -65537
+  end
+
+  test "load Fixnum -16777216" do
+    assert RubyMarshal.load(<<4, 8, 105, 253, 0, 0, 0>>) == -16777216
+  end
+
+  test "load Fixnum -16777217" do
+    assert RubyMarshal.load(<<4, 8, 105, 252, 255, 255, 255, 254>>) == -16777217
+  end
+
+  test "load Fixnum -4294967296" do
+    assert RubyMarshal.load(<<4, 8, 105, 252, 0, 0, 0, 0>>) == -4294967296
   end
 end
